d4cfcc63f2c89b89f29580a8d933dbf5146983e0
obaidul-islam <obaidul15-2255@diu.edu.bd>
2023-02-10 18:14:34 +0600

    Layout Setup

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\HomeComponent;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/',HomeComponent::class)->name('home.index');

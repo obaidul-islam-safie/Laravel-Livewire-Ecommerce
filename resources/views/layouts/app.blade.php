18fc2a0f051e67e656d7fca7427c5a645c9dd0ff
obaidul-islam <obaidul15-2255@diu.edu.bd>
2023-02-13 22:10:31 +0600

    Add Admin and User Authentication

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,9 +52,22 @@
                     </div>
                     <div class="col-xl-3 col-lg-4">
                         <div class="header-info header-info-right">
+                            @auth
+                                
                             <ul>                                
-                                <li><i class="fi-rs-key"></i><a href="login.html">Log In </a>  / <a href="register.html">Sign Up</a></li>
+                                <li><i class="fi-rs-user"></i> {{Auth::user()->name}}  / 
+                                    <form method="POST" action="{{route('logout')}}">
+                                        @csrf
+                                        <a href="{{route('logout')}}" onclick="even.preventDefault(); this.closest('form').submit();" >Logout</a>
+                                    </form>
+                                </li>
                             </ul>
+                           
+                            @else
+                            <ul>                                
+                                <li><i class="fi-rs-key"></i><a href="{{route('login')}}">Log In </a>  / <a href="{{route('register')}}">Sign Up</a></li>
+                            </ul>
+                            @endif
                         </div>
                     </div>
                 </div>
@@ -383,15 +396,22 @@
                                     <li><a href="blog.html">Blog </a></li>                                    
                                     <li><a href="contact.html">Contact</a></li>
                                     <li><a href="#">My Account<i class="fi-rs-angle-down"></i></a>
-                                        <ul class="sub-menu">
-                                            <li><a href="#">Dashboard</a></li>
-                                            <li><a href="#">Products</a></li>
-                                            <li><a href="#">Categories</a></li>
-                                            <li><a href="#">Coupons</a></li>
-                                            <li><a href="#">Orders</a></li>
-                                            <li><a href="#">Customers</a></li>
-                                            <li><a href="#">Logout</a></li>                                            
-                                        </ul>
+                                        @auth
+                                            @if(Auth::user()->utype == 'ADM')
+                                                <ul class="sub-menu">
+                                                    <li><a href="{{route('admin.dashboard')}}">Dashboard</a></li>
+                                                    <li><a href="#">Products</a></li>
+                                                    <li><a href="#">Categories</a></li>
+                                                    <li><a href="#">Coupons</a></li>
+                                                    <li><a href="#">Orders</a></li>
+                                                    <li><a href="#">Customers</a></li>                                         
+                                                </ul>
+                                             @else
+                                                    <ul class="sub-menu">
+                                                        <li><a href="{{route('user.dashboard')}}">Dashboard</a></li>                                            
+                                                    </ul>
+                                            @endif
+                                        @endif
                                     </li>
                                 </ul>
                             </nav>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\HomeComponent;
 
 use App\Http\Livewire\ShopComponent;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\User\UserDashboardComponent;
+use App\Http\Livewire\Admin\AdminDashboardComponent;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -18,11 +22,43 @@ use App\Http\Livewire\CheckoutComponent;
 |
 */
 
-
-
 Route::get('/',HomeComponent::class)->name('home.index');
 
 
 Route::get('/shop',ShopComponent::class)->name('shop');
 Route::get('/cart',CartComponent::class)->name('shop.cart');
 Route::get('/checkout',CheckoutComponent::class)->name('shop.checkout');
+
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/user/dashboard', UserDashboardComponent::class)->name('user.dashboard');
+
+});
+
+Route::middleware(['auth','authadmin'])->group(function () {
+
+    Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
+
+});
+
+
+
+
+
+
+
+
+
+
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
